11e1f35a9b161f2972f27d7b6ee081c2d129d447
gpenverne <gpenverne@gmail.com>
2019-09-26 11:16:12 +0200

    debug colors

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,24 +1,27 @@
 const config = require('../config.json');
-const knownData = require('../data.json');
+const localData = require('../data.json');
 const parser = require('../parsers/'+config.trello.parser+'.js')
 const request = require('sync-request');
 const fs = require('fs');
+
+const FgGreen = "\x1b[32m"
+const FgRed = "\x1b[31m"
+const FgCyan = "\x1b[36m"
 
 boardId = config.trello.board_id;
 apiKey = config.trello.api_key;
 oauthToken = config.trello.oauth_token;
 
 const Trello = require('trello-node-api')(apiKey, oauthToken);
-let localData = {};
 let collectionData = [];
 var res = request('GET', `https://api.trello.com/1/board/${boardId}/lists?key=${apiKey}&token=${oauthToken}`);
 var data = JSON.parse(res.getBody('utf8'));
 var lists = {};
 var knownItems = [];
 
-for (var prop in knownData) {
-    if (Object.prototype.hasOwnProperty.call(knownData, prop)) {
-        knownItems.push(knownData[prop].cardId)
+for (var prop in localData) {
+    if (Object.prototype.hasOwnProperty.call(localData, prop)) {
+        knownItems.push(localData[prop].cardId)
     }
 }
 
@@ -33,23 +36,25 @@ data.forEach(function(list){
 Trello.board.searchCards(boardId).then(function(response) {
 	response.forEach(function(cardResponse){
 		if (knownItems.indexOf(cardResponse.id) > -1) {
+			console.log(FgCyan+cardResponse.name+' already known, skipping...');
 			return;
 		}
 		card = parser.fetchCard(Trello, cardResponse);
-		if (!card || !card.cover) {
-			console.log('Unable to find `'+cardResponse.name+'`');
+		if (!card) {
+			console.log(FgCyan+'Unable to find `'+cardResponse.name+'`');
             return;
 		}
-        try {
+        if (card.cover) {
+			console.log(FgGreen+'Generating picture');
     		var res = request('POST', `https://api.trello.com/1/cards/${card.id}/attachments?key=${apiKey}&token=${oauthToken}`, {
     			json: {
     				url: card.cover
     			}
     		});
-        }
-        catch(e) {
-            return;
-        }
+        } else {
+			console.log(FgCyan+'No picture found');
+			card.cover = null;
+		}
 		var data = JSON.parse(res.getBody('utf8'));
         localData[cardResponse.name] = {
             'cardId': cardResponse.id,
@@ -60,9 +65,12 @@ Trello.board.searchCards(boardId).then(function(response) {
 			'parserId': card.parserId
         }
 		collectionData.push(localData[cardResponse.name]);
-        cardResponse.idAttachmentCover = data['id'];
+		if (data['id']) {
+        	cardResponse.idAttachmentCover = data['id'];
+		}
 		cardResponse.desc = card.desc;
         Trello.card.update(cardResponse.id, cardResponse);
+		console.log(FgGreen+cardResponse.name+' updated!');
 
 	});
     fs.writeFileSync('data.json', JSON.stringify(localData, null, 2));
